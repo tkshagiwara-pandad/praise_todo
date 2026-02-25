962ff7dbf9509d1b26742b8aaaf7fd06152e2bf1
萩原崇 <hagiwaratakashi@TH2023.local>
2026-02-25 20:05:13 +0900

    deploy: off-screen persistent input for fire Safari focus

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b84eb66621895ba9c56c1dc1f91297a1",
+const RESOURCES = {"flutter_bootstrap.js": "8ddba69aea7652e7b75f077d46afbaef",
 "version.json": "cfb8d453c8cf0b29dd96f96f33be9938",
 "index.html": "be36f03540235953e8f5f9dc00349bfe",
 "/": "be36f03540235953e8f5f9dc00349bfe",
-"main.dart.js": "77ad3d02a31c551ac8578663dd790ee6",
+"main.dart.js": "1ae406076659ca2cc9c00363e124d535",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "afe51dafaea8c615ef977fb17adcd6cd",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "b84eb66621895ba9c56c1dc1f91297a1",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "ed79f8ac9b3d25fd946e4fa180da74f4",
+"assets/fonts/MaterialIcons-Regular.otf": "b19fb7a1e7110a5673d87ff583424d7d",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
